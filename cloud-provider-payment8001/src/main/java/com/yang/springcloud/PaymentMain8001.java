8144b3719ad9036da54d10e11bea073b4b6d68e9
SoftLeaderGy <826044698@qq.com>
2022-04-15 13:54:42 +0800

    创建PaymentMain8001服务

---
+++ cloud-provider-payment8001/src/main/java/com/yang/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.yang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: Guo.Yang
+ * @Date: 2022/04/14/23:33
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
