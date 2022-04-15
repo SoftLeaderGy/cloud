771761d9387c6e593ad6b5da9e0cae86cd9902c8
SoftLeaderGy <826044698@qq.com>
2022-04-15 14:02:16 +0800

    建表sql提交

---
+++ cloud-provider-payment8001/src/main/resources/sql/his_sql.sql
@@ -0,0 +1,6 @@
+-- 创建支付表
+CREATE TABLE `payment` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `serial` varchar(200) DEFAULT '',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
