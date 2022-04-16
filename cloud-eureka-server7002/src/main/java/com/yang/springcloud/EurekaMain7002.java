9dfa79fa198b5b58ac621bc1ddc82b159bffa0fc
SoftLeaderGy <826044698@qq.com>
2022-04-17 00:20:58 +0800

    创建7002服务 搭建eureka集群服务

---
+++ cloud-eureka-server7002/src/main/java/com/yang/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.yang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description:
+ * @Author: Guo.Yang
+ * @Date: 2022/04/16/21:53
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
