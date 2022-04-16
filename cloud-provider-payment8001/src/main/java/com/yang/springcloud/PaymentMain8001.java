528c1af57b68449babf88295abcbf96aa58d5b5b
SoftLeaderGy <826044698@qq.com>
2022-04-16 22:58:44 +0800

    cloud-provider-payment8001注册进eureka中

--- cloud-provider-payment8001/src/main/java/com/yang/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/yang/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.yang.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Description:
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2022/04/14/23:33
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
