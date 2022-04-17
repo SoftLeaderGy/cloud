63ddbf65e96e8f49c07609b1b2f8767f21217fec
SoftLeaderGy <826044698@qq.com>
2022-04-18 00:19:41 +0800

    服务发现Discovery

--- cloud-provider-payment8001/src/main/java/com/yang/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/yang/springcloud/controller/PaymentController.java
@@ -3,8 +3,13 @@ package com.yang.springcloud.controller;
 import com.yang.springcloud.entity.CommonResult;
 import com.yang.springcloud.entity.Payment;
 import com.yang.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @Description:
@@ -13,11 +18,14 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/payment")
+@Slf4j
 public class PaymentController {
 
     @Autowired
     private PaymentService paymentService;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
     @PostMapping("/create")
     public CommonResult create(@RequestBody Payment payment){
         return paymentService.create(payment);
@@ -27,4 +35,20 @@ public class PaymentController {
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         return paymentService.getPaymentById(id);
     };
+    @GetMapping("/getPaymentById/discovery")
+    public Object discovery(){
+        // 获取注册进eureka的所有服务
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("service: "+ service);
+        }
+
+        // 获取某个服务中的所有实例信息
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info("实例："+ instance);
+            log.info("服务主机："+ instance.getHost() + "端口："+ instance.getPort() + "地址：" + instance.getUri());
+        }
+        return discoveryClient;
+    }
 }
