004d4f9f998b6370015079ed8e5afdd5b1b261da
SoftLeaderGy <826044698@qq.com>
2022-04-19 09:36:29 +0800

    测试restTemplate 的 ForEntity方法

--- cloud-consumer-order80/src/main/java/com/yang/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/yang/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.yang.springcloud.entity.CommonResult;
 import com.yang.springcloud.entity.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,8 +29,36 @@ public class OrderController {
         return restTemplate.postForObject(PAYMENT_URL+ "/payment/create",payment,CommonResult.class);
     }
 
+    /**
+     * restTemplate的postForEntity方法测试
+     * @param payment
+     * @return
+     */
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create1(Payment payment){
+        ResponseEntity<CommonResult> commonResultResponseEntity = restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        CommonResult body = commonResultResponseEntity.getBody();
+        return body;
+    }
+
+
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL + "/payment/getPaymentById/" + id ,CommonResult.class);
+    }
+
+    /**
+     * restTemplate的getForEntity方法测试
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/getPayment2/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/getPaymentById/" + id, CommonResult.class);
+        if (forEntity.getStatusCode().is2xxSuccessful()){
+            return forEntity.getBody();
+        }else {
+            return new CommonResult(444,"操作失败");
+        }
     }
 }
