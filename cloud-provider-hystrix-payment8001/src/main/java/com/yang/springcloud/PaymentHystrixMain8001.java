a2f929d74a810a38b3c5eb65d37e906c4c488b6c
SoftLeaderGy <826044698@qq.com>
2022-04-27 20:35:07 +0800

    将hystrix8001服务 中的延时报错方法添加"兜底方法"  实现服务降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/yang/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/yang/springcloud/PaymentHystrixMain8001.java
@@ -2,7 +2,9 @@ package com.yang.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @Description:
@@ -11,6 +13,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+//@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
--- cloud-provider-hystrix-payment8001/src/main/java/com/yang/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/yang/springcloud/service/PaymentService.java
@@ -1,6 +1,9 @@
 package com.yang.springcloud.service;
 
 import java.util.concurrent.TimeUnit;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
@@ -27,10 +30,23 @@ public class PaymentService {
      * @return
      */
     @SneakyThrows
+    @HystrixCommand(fallbackMethod = "paymentInfo_timeOutFallback",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String paymentInfo_timeOut(Integer id){
-        Integer timeOutNum = 3;
+        Integer timeOutNum = 5;
         // 延时3秒
         TimeUnit.SECONDS.sleep(3);
         return "线程池" + Thread.currentThread().getName() + "paymentInfo_timeOut ,id : " + id + "延时了" + timeOutNum + "秒";
+    }
+
+    /**
+     * @HystrixCommand 注解里边配置的fallbackMethod 方法
+     * 也就是说 当被标记的方法出问题了，就会降级到对应配置的降级方法
+     * @param id
+     * @return
+     */
+    public String paymentInfo_timeOutFallback(Integer id){
+        return "线程池" + Thread.currentThread().getName() + "系统繁忙，请稍后再试 ,id : " + id ;
     }
 }
