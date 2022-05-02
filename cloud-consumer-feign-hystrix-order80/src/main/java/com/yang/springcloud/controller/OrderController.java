93023ec7d93d1db43d3e6c85ef737dc3a7a121bf
SoftLeaderGy <826044698@qq.com>
2022-05-02 19:41:26 +0800

    配置全局的服务降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/yang/springcloud/controller/OrderController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/yang/springcloud/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.yang.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.yang.springcloud.feign.OrderFeignService;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2022/04/26/22:06
  */
 @RestController
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod") // 配置全局的服务降级
 public class OrderController {
 
     @Autowired
@@ -32,9 +34,10 @@ public class OrderController {
      * @return
      */
     @RequestMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "payment_timeoutFallback",commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1000")
-    })
+//    @HystrixCommand(fallbackMethod = "payment_timeoutFallback",commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1000")
+//    })
+    @HystrixCommand //加了@DefaultProperties属性注解，并且没有写具体方法名字，就用统一全局的
     public String payment_timeout(@PathVariable("id") Integer id){
 //        int a = 10/0;
         return orderFeignService.payment_timeout(id);
@@ -42,5 +45,9 @@ public class OrderController {
 
     public String payment_timeoutFallback(@PathVariable("id") Integer id){
         return "我是消费者 80, 对方支付系统繁忙请 10 秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o ";
+    }
+
+    public String payment_Global_FallbackMethod() {
+        return"Global 异常处理信息，请稍后再试， /( ㄒ o ㄒ )/~~" ;
     }
 }
