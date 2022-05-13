4d4940fcef6bbec445576090bffb46fe41a24936
SoftLeaderGy <826044698@qq.com>
2022-05-13 19:36:38 +0800

    创建配置中心服务ConfigCenterMain3344，并读取部署在远端服务器上的配置（本次放在github上）

---
+++ cloud-config-center-3344/src/main/java/com/yang/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.yang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Description: 服务配置中心
+ * @Author: Guo.Yang
+ * @Date: 2022/05/12/20:38
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
