48cef1c5f86a267ce321ed43757647e7fb590ccd
SoftLeaderGy <826044698@qq.com>
2022-05-14 17:52:11 +0800

    闯将访问配置中心的苦户端服务 ConfigClientMain3355  访问3344服务  获取配置

---
+++ cloud-config-client-3355/src/main/java/com/yang/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.yang.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: Guo.Yang
+ * @Date: 2022/05/14/17:32
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/getConfigInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
