6d242092e2565fe83b50249600ee93a3e9592959
SoftLeaderGy <826044698@qq.com>
2022-05-15 17:01:11 +0800

    演示广播效果，增加复杂度，再以3355为模板再制作一个3366

---
+++ cloud-config-client-3366/src/main/java/com/yang/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.yang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description:
+ * @Author: Guo.Yang
+ * @Date: 2022/05/15/16:38
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/yang/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.yang.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: Guo.Yang
+ * @Date: 2022/05/15/16:38
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("server.port")
+    private String port;
+
+    @Value("config.info")
+    private String configInfo;
+
+    @GetMapping("/getConfigInfo")
+    public String getConfigInfo(){
+        return "Server port: " + port + "Config Info: "  + configInfo;
+    }
+}
