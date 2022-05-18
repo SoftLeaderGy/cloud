a0352e67c54b5b6486e59edc062241f480ccac8c
SoftLeaderGy <826044698@qq.com>
2022-05-18 11:11:55 +0800

    创建nacos配置中心的客户端服务 cloudalibaba-config-nacos-client3377，并读取nacos配置中心的配置文件，

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/yang/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.yang.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: Guo.Yang
+ * @Date: 2022/05/17/17:30
+ */
+@RestController
+@RefreshScope // 在控制器类加入 @RefreshScope 注解使当前类下的配置支持 Nacos 的动态刷新功能。
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
